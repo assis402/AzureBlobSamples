846f0a13a87a2731f30b7e4c69b62ec6c30aa3e7
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-07-30 19:36:54 -0300

    Lease and Metadata

---
+++ BlobLeaseApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccassis204;AccountKey=nupkX1VHcPNXvPBgzNRWOAA4m18tSSAAWy8c33PAxxgx5WAShWg7C/2xXb3xfLqy/Fe4jL3C/aVu+ASt0vXqxg==;EndpointSuffix=core.windows.net";
+var containerName = "scripts";
+
+//await SetBlobMetadata();
+await AcquireLease();
+
+async Task AcquireLease()
+{
+    var blobName = "scripts.sql";
+    var blobClient = new BlobClient(connectionString, containerName, blobName);
+    var blobLeaseClient = blobClient.GetBlobLeaseClient();
+
+    var leaseTime = new TimeSpan(0, 0, 1, 00);
+
+    var response = await blobLeaseClient.AcquireAsync(leaseTime);
+
+    Console.Write("Lease id is: " + response.Value.LeaseId);
+}
---
+++ BlobMetadataApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccassis204;AccountKey=nupkX1VHcPNXvPBgzNRWOAA4m18tSSAAWy8c33PAxxgx5WAShWg7C/2xXb3xfLqy/Fe4jL3C/aVu+ASt0vXqxg==;EndpointSuffix=core.windows.net";
+var containerName = "scripts";
+
+//await SetBlobMetadata();
+await GetMetadata();
+
+async Task SetBlobMetadata()
+{
+    var blobName = "scripts.sql";
+    var blobClient = new BlobClient(connectionString, containerName, blobName);
+
+    IDictionary<string, string> metadata = new Dictionary<string, string>();
+    metadata.Add("Departament", "Logistics");
+    metadata.Add("Application", "AppA");
+
+    await blobClient.SetMetadataAsync(metadata);
+
+    Console.Write("Metadata added");
+}
+
+async Task GetMetadata()
+{
+    var blobName = "scripts.sql";
+    var blobClient = new BlobClient(connectionString, containerName, blobName);
+    BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+
+    foreach (var metadata in blobProperties.Metadata)
+    {
+        Console.WriteLine("The key is: " + metadata.Key);
+        Console.WriteLine("The value is: " + metadata.Value);
+    }
+}
