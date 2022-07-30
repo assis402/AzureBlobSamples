8ed5ccef23de617e99e9f1b95f305e5cab32ec08
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-07-30 18:54:56 -0300

    second commit

---
+++ BlobDownloadApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccassis204;AccountKey=nupkX1VHcPNXvPBgzNRWOAA4m18tSSAAWy8c33PAxxgx5WAShWg7C/2xXb3xfLqy/Fe4jL3C/aVu+ASt0vXqxg==;EndpointSuffix=core.windows.net";
+var containerName = "scripts";
+var blobName = "scripts.sql";
+var filePath = "C:\\tmp1\\script.sql";
+
+var blobClient = new BlobClient(connectionString, containerName, blobName);
+blobClient.DownloadToAsync(filePath);
+
+Console.WriteLine("The blob is downloaded");
