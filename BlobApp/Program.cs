5db5bd6f395e62fc2ecf68feb558353f143d4940
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-07-30 18:46:20 -0300

    first commit

---
+++ BlobApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccassis204;AccountKey=nupkX1VHcPNXvPBgzNRWOAA4m18tSSAAWy8c33PAxxgx5WAShWg7C/2xXb3xfLqy/Fe4jL3C/aVu+ASt0vXqxg==;EndpointSuffix=core.windows.net";
+var containerName = "scripts";
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
+
+Console.WriteLine("Container created");
---
+++ BlobListApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Storage.Blobs;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccassis204;AccountKey=nupkX1VHcPNXvPBgzNRWOAA4m18tSSAAWy8c33PAxxgx5WAShWg7C/2xXb3xfLqy/Fe4jL3C/aVu+ASt0vXqxg==;EndpointSuffix=core.windows.net";
+var containerName = "data";
+
+var blobContainerClient = new BlobContainerClient(connectionString, containerName);
+
+await foreach(var blobItem in blobContainerClient.GetBlobsAsync())
+{
+    Console.WriteLine("The blob name is:" + blobItem.Name);
+    Console.WriteLine("The blob size is:" + blobItem.Properties.ContentLength);
+} 
